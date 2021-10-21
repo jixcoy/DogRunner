67a2a25327d8ec67df478a3ce529015fc37da3d6
jixcoy <89876028+jixcoy@users.noreply.github.com>
2021-10-21 14:29:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,20 @@ Optional:
 - Music/Sound Effects for coins and jumps
 """
 import pygame
-from player import Player
+from classes import Player
 
 
+'#Background scroll variables'
+game_speed = 0
+bg_x_pos = 0
+bg_y_pos = 0
+
+
+def background():
+    speed = 10
+    return speed
+
+'#Setup'
 pygame.init()
 pygame.font.init()
 
@@ -30,22 +41,35 @@ WIDTH, HEIGHT = 543, 257
 WHITE = 255, 255, 255
 FPS = 60
 
+'#Background and Title setup variables'
 canvas = pygame.Surface((WIDTH, HEIGHT))
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 TITLE = FONT.render('RUNNER', False, (0, 0, 0))
 TITLE_rect = TITLE.get_rect(center = (WIDTH/2, HEIGHT/3))
 
-BACKGROUND = pygame.image.load('Assets/pixelart background.jpg').convert_alpha()
+pole = pygame.image.load('Assets/bush-1.png.png').convert_alpha()
+
+BACKGROUND = pygame.image.load('Assets/scrolling_background.jpg').convert_alpha()
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
-#Load Player
+'#Load Player'
 dog = Player()
 
+'#Game Loop'
 run = True
 while run:
 
+    '#Background and Title Scroll Logic'
+    bg_x_pos -= game_speed
+    TITLE_rect.x -= game_speed
+    if bg_x_pos <= -WIDTH:
+        bg_x_pos += WIDTH
 
+    canvas.blit(BACKGROUND, (bg_x_pos, bg_y_pos))
+    canvas.blit(TITLE, TITLE_rect)
+
+    '#Event Detection'
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -53,12 +77,14 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 dog.SPACE_KEY = True
+                '#Starts background scroll'
+                game_speed = background()
 
 
     dog.update()
-    canvas.blit(BACKGROUND, (0, 0))
     dog.draw(canvas)
+
     WIN.blit(canvas, (0, 0))
-    WIN.blit(TITLE, TITLE_rect)
+    WIN.blit(pole, (300, 150))
     clock.tick(FPS)
     pygame.display.update()
